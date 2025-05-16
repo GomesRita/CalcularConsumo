d58159257d14fea929aa5cd7c6eec520860d140b
GomesRita <gomesrita541@gmail.com>
2025-05-16 17:44:38 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { TextInput, Text, Button } from 'react-native-paper';
+import { useState } from 'react';
+import { StyleSheet, View} from 'react-native';
 
 export default function App() {
+  const [text, setText] = React.useState("");
+  const [text2, setText2] = React.useState("");
+  const [litros, setLitros] = useState<number>(0)
+  const [km, setKm] = useState<number>(0)
+  const [consumo, setConsumo] = useState<any>(0)
+
+  const calcular = (): any => {
+      if( km == 0 && litros == 0){
+        setConsumo(0)
+      }if( km == 0 && litros > 0){
+        setConsumo('Informe os km rodados')
+      }
+      else{
+        setConsumo(km/litros)
+      } 
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+       {consumo !== null && <Text style={styles.text} variant="headlineLarge" >{consumo}</Text>}
+      <TextInput placeholder='Km rodados'  style={styles.input} keyboardType='numeric' value={text} onChangeText={text => {setText(text); setKm(parseInt(text) || 0)}}/>
+      <TextInput placeholder='Litros gastos' style={styles.input}  keyboardType='numeric' value={text2} onChangeText={text => {setText2(text); setLitros(parseInt(text) || 0)}}/>  
+      <Button mode="contained" onPress={calcular}> Calcular </Button>
     </View>
   );
 }
@@ -17,4 +37,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    width: 200,
+    height: 50,
+    marginBottom: 10
+  },
+  text:{
+    marginBottom: 10
+  }
 });
